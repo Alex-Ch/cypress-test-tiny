fc0b09e6b62581d807e43a3d5c9a571c881f592f
Alex-Ch <53617402+Alex-Ch@users.noreply.github.com>
2019-08-04 22:00:20 +0300

    Add files via upload
    test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,5 +3,9 @@ describe('page', () => {
     cy.visit('http://storybook.b360-dev.autodesk.com/current/iframe.html?id=dropdowntree--default');
   })
 
-  it('works', () => {})
+  it('works', () => {
+	  cy.get('#root > div > div > div > div > div').click();
+	//  cy.get('#root > div > div > div.DropdownTree__tree > div > ul > li.Tree__tree-container > div > div > div > div > div:nth-child(1) > div > div > div > div > svg').click();
+	  cy.get('#root > div > div > div.DropdownTree__tree > div > ul > li.Tree__tree-container > div > div > div > div > div:nth-child(1) > div > div > div > div > svg').children().should('have.length', 1);
+  })
 })
